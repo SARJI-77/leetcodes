21cf11aa7af9ae06a8ec3ca27dd1256d27d1278b
moosa sarjid <moosasarjid77@gmail.com>
2026-08-18 10:42:12 +0530

    Time: 0 ms (100%), Space: 55.7 MB (65.32%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+            while (n > 1) {
+        if (n % 2 !== 0) {
+            return false;
+        }
+        n = n / 2;
+    }
+
+    return n === 1;
+
+};
